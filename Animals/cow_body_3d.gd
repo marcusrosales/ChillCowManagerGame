b48dd4fd273fee677ec4f6c1f97fb4d824d903b7
marcusrosales <marcusofficial5@gmail.com>
2025-09-02 00:16:51 -0500

    ai working

--- Animals/cow_body_3d.gd
+++ Animals/cow_body_3d.gd
@@ -1,31 +1,24 @@
 extends CharacterBody3D
+@onready var CowBrain = $NavigationAgent3D
+@export var RoamRegion: NavigationRegion3D
 
+var speed = 50
+var health = 100
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+func getRandomPoint():
+	var random_pos := Vector3.ZERO
+	random_pos.x = randf_range(-5.0,5.0)
+	random_pos.z = randf_range(-5.0,5.0)
+	CowBrain.set_target_position(random_pos)
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+func _ready():
+	getRandomPoint()
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	var destination = CowBrain.get_next_path_position()
+	var local_des = destination- global_position
+	var direction = local_des
+	
+	velocity = direction * speed * delta
+	
 	move_and_slide()
